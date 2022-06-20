9c0d7234fc870af759343fabd2fa54f5505ea13b
Robert Floor <robert.floor@rubix.nl>
2022-06-20 12:04:21 +0200

    minor fixes

--- src/main/java/com/ilionx/carapp/api/HelloworldController.java
+++ src/main/java/com/ilionx/carapp/api/HelloworldController.java
@@ -1,2 +1,16 @@
-package com.ilionx.carapp.api;public class HelloworldController {
+package com.ilionx.carapp.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/helloworld")
+public class HelloworldController {
+    @GetMapping
+    public String sayHello() {
+
+
+        return "Hello world";
+    }
 }
