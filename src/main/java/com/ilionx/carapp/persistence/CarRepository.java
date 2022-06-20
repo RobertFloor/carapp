40e7a06a925da2d1f4fb687858348b953893cbad
Robert Floor <robert.floor@rubix.nl>
2022-06-20 16:12:05 +0200

    added JPA stuff

---
+++ src/main/java/com/ilionx/carapp/persistence/CarRepository.java
@@ -0,0 +1,15 @@
+package com.ilionx.carapp.persistence;
+
+import com.ilionx.carapp.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+    public List<Car> findByBrand(String brand);
+    List<Car> findByLicensePlate (String licensePlate);
+
+    public List<Car> findByLicensePlateAndBrand(String brand, String licensePlate);
+}
