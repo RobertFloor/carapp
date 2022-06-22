98cec908ab62f689dcc056d9e07caf59798662d8
Robert Floor <robert.floor@rubix.nl>
2022-06-22 16:05:51 +0200

    added unit tests

---
+++ src/test/java/com/ilionx/carapp/model/CarTest.java
@@ -0,0 +1,31 @@
+package com.ilionx.carapp.model;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    // class under test
+    private Car car;
+
+
+    @BeforeEach
+    public void init() {
+        this.car = new Car();
+    }
+
+    @Test
+    public void testSetGetBrand() {
+
+        car.setBrand("Ferrari");
+        Assertions.assertEquals("Ferrari", car.getBrand());
+    }
+
+    @Test
+    public void testSetGetMileage() {
+        car.setMileage(11000);
+        Assertions.assertEquals(11000, car.getMileage());
+    }
+}
